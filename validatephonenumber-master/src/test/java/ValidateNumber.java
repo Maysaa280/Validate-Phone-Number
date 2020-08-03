3d992a03e652b3599a56d539678fcd68a3be03b8
Phirra91 <p.ndimlana@gmail.com>
2020-08-03 18:06:56 +0200

    Update Phone Numbers

--- validatephonenumber-master/src/test/java/ValidateNumber.java
+++ validatephonenumber-master/src/test/java/ValidateNumber.java
@@ -5,17 +5,17 @@ public class ValidateNumber
 {
     @Test(description = "Test to validate correct cellphone number")
     public void validateCorrectNumber(){
-        Assert.assertTrue(validation.validateSAPhoneNumber("0765182977"));
+        Assert.assertTrue(validation.validateSAPhoneNumber("0837077660"));
     }
 
     @Test(description = "This test validates phone number with country code")
     public void validateCorrectNumberCountryCode(){
-        Assert.assertTrue(validation.validateSAPhoneNumber("+27765182977"));
+        Assert.assertTrue(validation.validateSAPhoneNumber("+27837077660"));
     }
 
     @Test(description = "Test to validate phone number with country code without +")
     public void validateCorrectNumberCountryCodeWithoutPlus(){
-        Assert.assertTrue(validation.validateSAPhoneNumber("27765182977"));
+        Assert.assertTrue(validation.validateSAPhoneNumber("27837077660"));
     }
 
     @Test(description = "Test to validate phone number with invalid numbers")
@@ -25,16 +25,16 @@ public class ValidateNumber
 
     @Test(description = "Test to validate phone number with Alphanumerics")
     public void validateIncorrectNumberAlphaNumeric(){
-        Assert.assertFalse(validation.validateSAPhoneNumber("@#$rhdh55d"));
+        Assert.assertFalse(validation.validateSAPhoneNumber("pj*764fsjj@#%"));
     }
 
     @Test(description = "This test valid phonenumber length with less than 10 numbers")
     public void validateIncorrectNumberLessThanTen(){
-        Assert.assertFalse(validation.validateSAPhoneNumber("0765182"));
+        Assert.assertFalse(validation.validateSAPhoneNumber("083707"));
     }
 
     @Test(description = "This test valid phonenumber length with more than 10 numbers")
     public void validateIncorrectNumberMoreThanTen(){
-        Assert.assertFalse(validation.validateSAPhoneNumber("07651829777"));
+        Assert.assertFalse(validation.validateSAPhoneNumber("083707766023456"));
     }
 }
